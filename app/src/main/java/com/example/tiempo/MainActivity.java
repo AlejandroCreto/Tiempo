002ab7652337f81196eb930073cd2b3e9812e64d
sergiosaec11 <56092815+sergiosaec11@users.noreply.github.com>
2020-01-22 17:01:59 +0100

    COMENTARIO EN EL ONCREATE

--- app/src/main/java/com/example/tiempo/MainActivity.java
+++ app/src/main/java/com/example/tiempo/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //FDKNBSHHHHHHHBDBKBSHBFJSDHCNBSWHKCBV WMBHJCKNHW EJVXGHNBMDC,CNX FVGCHZVNMBCHGVH H VMBN XJV HJB
     }
 }
